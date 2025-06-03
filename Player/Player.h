a54feff6b27ba9e9fd271d0bd6483a30233c0d2c
elhatriayoub <elhatriayoub059@gmail.com>
2025-06-03 11:10:48 +0200

    version 1.1

--- Player/Player.h
+++ Player/Player.h
@@ -1,18 +1,22 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include "../StoneTiles/StoneTiles.h"
 class Player{
+    private:
+          unsigned int playerID ;
+          Set playerDeck ;
+          vector<shared_ptr<StoneTiles>> stoneTiles ;
+          unsigned int tacticalCardsCounter = 0;
+    public:
+
+
+      Player(unsigned int playerID, Set playerDeck);
+      virtual void playCard() = 0;
+      void revendicateStoneTiles();
+      void drawClanCards();  //piocher
+      void drawTacticalCards();
+
+
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
 #endif //PLAYER_H
