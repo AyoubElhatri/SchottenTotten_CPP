2acdb41ddced9977a1ba59bc12217420852a4398
ZTOUF <yahya.atouf@etu.univ-tours.fr>
2025-06-03 11:10:38 +0200

    Updates

---
+++ Collections/Set.h
@@ -0,0 +1,21 @@
+//
+// Created by atoum on 03/06/2025.
+//
+
+#ifndef SET_H
+#define SET_H
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include "../Cards/Cards.h"
+using namespace std;
+
+class Set {
+ public:
+  vector<unique_ptr<Cards>> SetOfCards;
+
+};
+
+
+
+#endif //SET_H
--- cmake-build-debug/CMakeFiles/SchottenTotten_CPP.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/SchottenTotten_CPP.dir/DependInfo.cmake
@@ -10,6 +10,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "C:/Users/atoum/CLionProjects/SchottenTotten_CPP/Cards/ClanCards.cpp" "CMakeFiles/SchottenTotten_CPP.dir/Cards/ClanCards.cpp.obj" "gcc" "CMakeFiles/SchottenTotten_CPP.dir/Cards/ClanCards.cpp.obj.d"
   "C:/Users/atoum/CLionProjects/SchottenTotten_CPP/Cards/Stratege.cpp" "CMakeFiles/SchottenTotten_CPP.dir/Cards/Stratege.cpp.obj" "gcc" "CMakeFiles/SchottenTotten_CPP.dir/Cards/Stratege.cpp.obj.d"
+  "C:/Users/atoum/CLionProjects/SchottenTotten_CPP/Collections/Set.cpp" "CMakeFiles/SchottenTotten_CPP.dir/Collections/Set.cpp.obj" "gcc" "CMakeFiles/SchottenTotten_CPP.dir/Collections/Set.cpp.obj.d"
   "C:/Users/atoum/CLionProjects/SchottenTotten_CPP/DisplayConsole.cpp" "CMakeFiles/SchottenTotten_CPP.dir/DisplayConsole.cpp.obj" "gcc" "CMakeFiles/SchottenTotten_CPP.dir/DisplayConsole.cpp.obj.d"
   "C:/Users/atoum/CLionProjects/SchottenTotten_CPP/Rules/Rules.cpp" "CMakeFiles/SchottenTotten_CPP.dir/Rules/Rules.cpp.obj" "gcc" "CMakeFiles/SchottenTotten_CPP.dir/Rules/Rules.cpp.obj.d"
   "C:/Users/atoum/CLionProjects/SchottenTotten_CPP/StoneTiles/StoneTiles.cpp" "CMakeFiles/SchottenTotten_CPP.dir/StoneTiles/StoneTiles.cpp.obj" "gcc" "CMakeFiles/SchottenTotten_CPP.dir/StoneTiles/StoneTiles.cpp.obj.d"
