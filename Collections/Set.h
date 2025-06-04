4d9809f21d78bf9904cbe42a490f50de056850cb
ZTOUF <yahya.atouf@etu.univ-tours.fr>
2025-06-04 09:38:10 +0200

    Updated Set.h

--- Collections/Set.h
+++ Collections/Set.h
@@ -8,12 +8,33 @@
 #include <algorithm>
 #include <memory>
 #include "../Cards/Cards.h"
+#include <iostream>
 using namespace std;
 
 class Set {
- public:
+ private:
   vector<unique_ptr<Cards>> SetOfCards;
+public:
+    unsigned int getSize()const {
+        return SetOfCards.size();
+    }
+    Set(Set&& IndexSet) {
+        for (unsigned int IndexLoop=0; IndexLoop < IndexSet.getSize(); IndexLoop++) {
 
+        }
+    }
+    unique_ptr<Cards> getCardbyIndex(unsigned int IndexParam){
+        if (IndexParam>=getSize()) {
+            cout<<"Cexception";
+        }
+        unique_ptr<Cards> IndexCard= move(SetOfCards[IndexParam]);
+        SetOfCards.erase(SetOfCards.begin()+IndexParam);
+        return move(IndexCard);
+    }
+    void addCard(unique_ptr<Cards> Card) {
+        SetOfCards.push_back(move(Card));
+    }
+    void moveCard(unsigned int IndexOfCard){}
 };
 
 
---
+++ cmake-build-debug/CMakeFiles/SchottenTotten_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SchottenTotten_CPP.dir/Cards/ClanCards.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/Cards/ClanCards.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/Cards/Stratege.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/Cards/Stratege.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/Collections/Set.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/Collections/Set.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/DisplayConsole.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/DisplayConsole.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/Rules/Rules.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/Rules/Rules.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/StoneTiles/StoneTiles.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/StoneTiles/StoneTiles.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/TacticalCardsFactory.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/TacticalCardsFactory.cpp.obj.d"
+  "CMakeFiles/SchottenTotten_CPP.dir/main.cpp.obj"
+  "CMakeFiles/SchottenTotten_CPP.dir/main.cpp.obj.d"
+  "SchottenTotten_CPP.exe"
+  "SchottenTotten_CPP.exe.manifest"
+  "SchottenTotten_CPP.pdb"
+  "libSchottenTotten_CPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SchottenTotten_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
