81b4cd367b5947232aef0954d7d71216206f58c8
ZTOUF <yahya.atouf@etu.univ-tours.fr>
2025-06-10 11:57:42 +0200

    Fixed some issues

--- Cards/EliteTroopsCards.h
+++ Cards/EliteTroopsCards.h
@@ -6,10 +6,10 @@
 #define ELITETROOPSCARDS_H
 #include "ClanCards.h"
 #include "TacticalCards.h"
-
+using namespace std;
 class EliteTroopsCards : public ClanCards, public TacticalCards{
-
-
+    string Name;
+    string getName(){return Name;}
 
 };
 
