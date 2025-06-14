e1b64ee2d1a4d362968a273b07e95d9a9ac93c54
ZTOUF <yahya.atouf@etu.univ-tours.fr>
2025-06-14 22:09:25 +0200

    Added new CustomException class for error handling
    The class is used by Joker.cpp to handle errors. Such a class was created for developers to have more control of what happens when Exceptions take place, such as wrong inputs.

--- Cards/Joker.cpp
+++ Cards/Joker.cpp
@@ -2,14 +2,28 @@
 
 #include "Joker.h"
 #include "../DisplayManager.h"
-
+#include "../CustomException/CustomException.h"
 
 void Joker::getEvent(StoneTiles* stoneTiles) {
     DisplayManager::getInstance()->output("What's the number of the card you want to play?");
-    unsigned int cardNumber = atoi(DisplayManager::getInstance()->takeInput().c_str());
+    try {
+        unsigned int cardNumber = stoi(DisplayManager::getInstance()->takeInput());
+        setNumber(cardNumber);
+    }
+    catch (...) {
+       CustomException Exc1=CustomException(INVALIDNUMBERINPUT);//INVALIDNUMBERINPUT defined within CustomException class.
+        throw(Exc1);
+    }
     DisplayManager::getInstance()->output("What's the color of the card you want to play?");
-    int colorInput = atoi(DisplayManager::getInstance()->takeInput().c_str());
-    Colors cardColor = static_cast<Colors>(colorInput);
-    setColor(cardColor);
-    setNumber(cardNumber);
+    try {
+        int colorInput = stoi(DisplayManager::getInstance()->takeInput());
+        Colors cardColor = static_cast<Colors>(colorInput);
+        setColor(cardColor);
+    }
+    catch (...) {
+        CustomException Exc2=CustomException(INVALIDCOLORINPUT);
+        throw(Exc2);
+    }
+
+
 }
