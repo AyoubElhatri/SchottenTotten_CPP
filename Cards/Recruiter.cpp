5e16a93a19c7ce9acedcfbdebf0af572ac21fea8
elhatriayoub <elhatriayoub059@gmail.com>
2025-06-16 13:55:14 +0200

    ajout claim + modification

--- Cards/Recruiter.cpp
+++ Cards/Recruiter.cpp
@@ -1,7 +1,35 @@
 #include "Recruiter.h"
 #include <iostream>
 
+#include "../GameBoard.h"
 
-Recruiter::Recruiter() {}
+
+void Recruiter::getEvent() {
+    int cardsToDraw = 3;
+    int draw = 0;
+
+    GameBoard* board = &GameBoard::getInstance();
+    while (cardsToDraw != 0) {
+        const Set& supraset1 = board->getRemainingTacticalCards();
+        DisplayManager::getInstance()->output("RemainingTacticalCards : \n");
+        supraset1.printSet();
+        const Set& supraset2 = board->getRemainingClanCards();
+        DisplayManager::getInstance()->output("RemainingClanCards : \n");
+        supraset2.printSet();
+        if (supraset1.getSize() != 0) {
+            if (
+            string message = "How many Tactical cards do you want ? (Number between 0 -")";
+            DisplayManager::getInstance()->output(message);
+
+        }
+
+
+
+
+    }
+
+
+
+}
 
 
