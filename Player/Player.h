f5849e346c3179cac047ae3e9f72894d468c5796
elhatriayoub <elhatriayoub059@gmail.com>
2025-06-16 11:15:45 +0200

    ajout claim + modification

--- Player/Player.h
+++ Player/Player.h
@@ -18,22 +18,13 @@ private:
 public:
     Player(unsigned int playerId, Set&& playerDecks);
     virtual ~Player() = default;
-
     unsigned int getPlayerID() const { return playerID; }
     Set& getPlayerDeck() { return playerDeck; }
-    //const Set& getPlayerDeck() const { return playerDeck; }
     const vector<shared_ptr<StoneTiles>>& getStoneTiles() const { return stoneTiles; }
-
     unsigned int getTacticalCardsCounter() const { return tacticalCardsCounter; }
-
     virtual void playCard(unique_ptr<Cards>) = 0;
-
-    // Méthode facultative, à définir ou supprimer
-    // bool claimStoneTiles(unsigned int position);
-
     void drawClanCards(unsigned int count = 1);
     void drawTacticalCards(unsigned int count = 1);  // définir si utilisée
-
     void incrementTacticalCardCounter() { tacticalCardsCounter++; }
 };
 
