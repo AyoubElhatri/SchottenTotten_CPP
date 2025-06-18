53f8878fa49d40c1cc869d6af623414ddb70f3be
elhatriayoub <elhatriayoub059@gmail.com>
2025-06-18 17:50:04 +0200

    Banshee

--- Player/AIEasy.cpp
+++ Player/AIEasy.cpp
@@ -1,36 +1,92 @@
 #include "AIEasy.h"
-#include"Player.h"
-#include <cstdlib>  // rand()
-#include <ctime>    // time()
+#include "../GameBoard/GameBoard.h"
+#include "../Cards/EliteTroopsCards.h"
+#include "../Cards/CombatModeCards.h"
+#include "../Cards/RusesCards.h"
+#include "../Logic/GameLogic.h"
+#include "../Display/DisplayManager.h"
+#include <random>
 
+void AIEasy::playCard() {
+    GameBoard& board = GameBoard::getInstance();
+    Set& deck = getPlayerDeck();
 
-AIEasy::AIEasy(unsigned int playerId)
-    : AI(playerId) {}
+    if (deck.getSize() == 0) return;
 
-void AIEasy::playTurn(vector<unique_ptr<Cards>>& playerCards, GameBoard& gameBoard) {
-    int cardIndex = chooseCardIndex(playerCards);
-    int tileIndex = chooseTileIndex(gameBoard);
+    int cardIndex = rand() % deck.getSize();
+    Cards* selectedCard = deck.getCardAt(cardIndex);
 
-    if (cardIndex == -1 || tileIndex == -1) return;
-    unique_ptr<Cards> selectedCard = std::move(playerCards[cardIndex]);
+    int tileIndex = -1;
 
-    // placeCardOnTile retourne void, donc on ne stocke pas de booléen ici
-    gameBoard.placeCardOnTileByIndexOfTheTile(tileIndex, *selectedCard, m_playerID);
+    if (auto eliteTroop = dynamic_cast<EliteTroopsCards*>(selectedCard)) {
+        do {
+            tileIndex = rand() % board.getBoardSize();
+        } while (board.getSharedTiles()[tileIndex]->isAlreadyClaimed());
 
-    // On supprime la carte du vecteur car elle est jouée
-    playerCards.erase(playerCards.begin() + cardIndex);
-}
-
-int AIEasy::chooseCardIndex(const vector<unique_ptr<Cards>>& cards) {
-    if (cards.empty()) return -1;
-    return rand() % cards.size();
-}
-
-int AIEasy::chooseTileIndex(const GameBoard& gameBoard) {
-    int boardSize = gameBoard.getBoardSize();
-    for (int tries = 0; tries < boardSize * 2; ++tries) {
-        int index = rand() % boardSize;
-        if (gameBoard.isTileFree(index)) return index;
+        board.placeCardOnTileByIndexOfTheTile(tileIndex, *selectedCard, getPlayerID());
     }
-    return -1;
+    else if (auto combatMode = dynamic_cast<CombatModeCards*>(selectedCard)) {
+        do {
+            tileIndex = rand() % board.getBoardSize();
+        } while (board.getSharedTiles()[tileIndex]->isAlreadyClaimed());
+
+        combatMode->getEvent(board.getSharedTiles()[tileIndex].get());
+        deck.moveCard(cardIndex, board.getSharedTiles()[tileIndex]->getCombatModeCards());
+    }
+    else if (auto ruse = dynamic_cast<RusesCards*>(selectedCard)) {
+        ruse->getEvent(nullptr);
+        deck.moveCard(cardIndex, board.getDiscardedCards());
+    }
+    else {
+        do {
+            tileIndex = rand() % board.getBoardSize();
+        } while (board.getSharedTiles()[tileIndex]->isAlreadyClaimed());
+
+        board.placeCardOnTileByIndexOfTheTile(tileIndex, *selectedCard, getPlayerID());
+    }
+
+    if (tileIndex != -1) {
+        auto& tile = board.getSharedTiles()[tileIndex];
+        if (tile->getPlayerCardsOnTilesByPlayerId(getPlayerID()).getSize() == tile->getNbOfPlayableCards()
+            && tile->getFirstPlayerToFillTheStoneTile() == nullptr) {
+            tile->setFirstPlayerToFillTheStoneTile(this);
+        }
+    }
+
+    // Piocher une carte aléatoire (si disponible)
+    bool canDrawClan = board.getRemainingClanCards().getSize() > 0;
+    bool canDrawTactical = board.getRemainingTacticalCards().getSize() > 0;
+
+    if (canDrawClan && canDrawTactical) {
+        if (rand() % 2 == 0) drawClanCards();
+        else drawTacticalCards();
+    } else if (canDrawClan) {
+        drawClanCards();
+    } else if (canDrawTactical) {
+        drawTacticalCards();
+    }
+}
+
+void AIEasy::playTurn() {
+    DisplayManager::getInstance()->output(" Tour de l'IA (joueur " + std::to_string(getPlayerID()) + ")\n");
+
+    GameBoard& board = GameBoard::getInstance();
+    int boardSize = board.getBoardSize();
+
+    // 20% de chance d'essayer de revendiquer une tuile
+    if (rand() % 100 < 20) {
+        for (int i = 0; i < boardSize; ++i) {
+            auto& tile = board.getSharedTiles()[i];
+            if (!tile->isAlreadyClaimed()) {
+                unsigned int id = getPlayerID();
+                tile->claim(id);
+                if (tile->isAlreadyClaimed()) {
+                    DisplayManager::getInstance()->output("L'IA a revendiqué la tuile " + std::to_string(i) + "\n");
+                    return;
+                }
+            }
+        }
+    }
+
+    playCard();
 }
